b440e3b3cd40e7883c44432f9017c0a8545622b0
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2018-08-07 13:25:30 -0700

    encode

--- trojan-url.py
+++ trojan-url.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-import json
+import sys
 import argparse
+import base64
+import json
 import qrcode
+from urllib.parse import urlparse
 
 DEFAULT_CONFIG = json.loads("""{"run_type":"client","local_addr":"127.0.0.1",
 "local_port":1080,"remote_addr":"example.com","remote_port":443,"password":[
@@ -13,6 +16,35 @@ DEFAULT_CONFIG = json.loads("""{"run_type":"client","local_addr":"127.0.0.1",
 "sigalgs":""},"tcp":{"keep_alive":true,"no_delay":true,"fast_open":true,
 "fast_open_qlen":5}}""")
 
+def fail(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+def encode(qr):
+    try:
+        config = json.load(sys.stdin)
+        if config['run_type'] != 'client':
+            fail('Please provide a client config')
+        if ':' in config['remote_addr']:
+            config['remote_addr'] = '[{}]'.format(config['remote_addr'])
+        url = 'trojan://{}@{}:{}'.format(base64.b64encode(config['password'][0].encode()).decode() ,config['remote_addr'], config['remote_port'])
+    except Exception as e:
+        fail('Invalid config')
+    try:
+        if qr:
+            qrcode.make(url).save(sys.stdout, 'PNG')
+        else:
+            print(url)
+    except:
+        fail('Save failed')
+
+def decode():
+    url = urlparse(sys.stdin.read().strip('\n'))
+    print(url)
+    if url.scheme != 'trojan':
+        fail('Not trojan URL')
+
+
 def main():
     parser = argparse.ArgumentParser(description='Encode and decode trojan URLs.')
     parser.add_argument('-d', '--decode', action='store_true', help='decode input')
@@ -20,7 +52,22 @@ def main():
     parser.add_argument('-i', '--input', default='-', metavar='in_file', help='input file (default: "-" for stdin)')
     parser.add_argument('-o', '--output', default='-', metavar='out_file', help='output file (default: "-" for stdout)')
     args = parser.parse_args()
-    print(args)
+    if args.qrcode:
+        if args.decode:
+            fail('Decoding QRCode is not supported')
+        if args.output == '-':
+            fail('Please output QRCode to a file')
+    if args.input != '-':
+        sys.stdin = open(args.input, 'r')
+    if args.output != '-':
+        if args.qrcode:
+            sys.stdout = open(args.output, 'wb')
+        else:
+            sys.stdout = open(args.output, 'w')
+    if args.decode:
+        decode()
+    else:
+        encode(args.qrcode)
 
 if __name__ == '__main__':
     main()
